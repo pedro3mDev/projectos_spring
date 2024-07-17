0c15f684e12f3edef270e8b61720c526580482b0
Pedro Moniz <88067976+pedro3mDev@users.noreply.github.com>
2024-07-17 10:26:01 +0100

    .
    .

---
+++ dataJpa/src/main/java/grupo3mtech/ao/dataJpa/DataJpaApplication.java
@@ -0,0 +1,13 @@
+package grupo3mtech.ao.dataJpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataJpaApplication.class, args);
+	}
+
+}
---
+++ dataJpa/src/test/java/grupo3mtech/ao/dataJpa/DataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package grupo3mtech.ao.dataJpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
